f03ab7e64f8f715bcc42b24d88146d9e0760c359
Guilherme Abade <abade.guim13@gmail.com>
2025-02-07 21:53:59 -0300

    Input e operadores matemáticos

---
+++ input.py
@@ -0,0 +1,7 @@
+# Recebendo dados do usuário
+senha = input('Digite sua seha: ')
+print(senha)
+print(type(senha))
+
+quantidade_de_filmes = int(input('Quantos filmes você já viu esse mês?'))
+print(type(quantidade_de_filmes))
---
+++ operacoes_numercas.py
@@ -0,0 +1,31 @@
+# Tipos de numeros que podem ser usados no Python
+
+import math
+a = 20
+b = 20.5
+print(type(a))  # Informa o tipo do valor informado (int)
+print(type(b))  # Informa o tipo do valor informado (float)
+
+# Operações Matemáticas
+print(10 + 6)
+print(10 - 6)
+print(10 * 6)
+print(10 / 6)
+print(10 // 6)  # divisão de inteiro
+print(10 % 6)  # modulus informa o resto da divisão
+print(10 ** 6)  # exponenciais
+
+# Atalho para atribuição mais rapida
+a = 10
+a = a + 5
+a += 5
+
+b = 20
+b = b - 2
+b -= 2
+
+# Operações matematicas comuns
+print(round(5.7))
+
+# Arredondando o 2.2 para o valor inteiro mais proximo acima
+print(math.ceil(2.2))
